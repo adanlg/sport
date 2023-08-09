f6556aefbf316e4a869715638146fba643561f49
adanlg <123789087+adanlg@users.noreply.github.com>
2023-08-09 07:35:05 +0200

    feat: calculate smalestChoiceIndex and more

--- copiSports.sol
+++ copiSports.sol
@@ -10,6 +10,7 @@ contract OddsCalculator is ReentrancyGuard, Ownable {
     uint256 public odds0;
 
     struct Bet {
+        address bettor;
         uint256 choice;
         uint256 amount;
     }
@@ -28,11 +29,11 @@ contract OddsCalculator is ReentrancyGuard, Ownable {
     bool public hasResult = false;
 
     mapping(address => Bet[]) public userBets;
+    Bet[] public allBets; // Array to store all bets
 
     mapping(address => uint) public personalPlacedBets1;
     mapping(address => uint) public personalPlacedBets2;
     mapping(address => uint) public personalPlacedBets0;
-
 
     constructor(uint256 _odds1, uint256 _odds2, uint256 _odds0, address _USDCTokenAddress) {
         odds1 = _odds1;
@@ -48,8 +49,12 @@ contract OddsCalculator is ReentrancyGuard, Ownable {
             "Not enough tokens"
         );
 
-        Bet memory newBet = Bet({ choice: _choice, amount: _amount });
-        userBets[msg.sender].push(newBet);
+        Bet memory newBet = Bet({
+            bettor: msg.sender,
+            choice: _choice,
+            amount: _amount
+        });
+        allBets.push(newBet); // Store the bet in the allBets array
 
         if (_choice == 1) {
             personalPlacedBets1[msg.sender] += _amount;
@@ -65,23 +70,72 @@ contract OddsCalculator is ReentrancyGuard, Ownable {
         } else {
             revert("Invalid bet");
         }
-        IERC20(USDCTokenAddress).transferFrom(address(msg.sender),address(this), _amount);
-
+        IERC20(USDCTokenAddress).transferFrom(address(msg.sender), address(this), _amount);
     }
+
+
+    function getChoiceSums() public view returns (uint256 smallestChoice,uint256 lastBetIndex, uint256 product1, uint256 product2, uint256 product0) {
+        uint256 sumChoice1;
+        uint256 sumChoice2;
+        uint256 sumChoice0;
+        
+        for (uint256 i = 0; i < allBets.length; i++) {
+            if (allBets[i].choice == 1) {
+                sumChoice1 += allBets[i].amount;
+            } else if (allBets[i].choice == 2) {
+                sumChoice2 += allBets[i].amount;
+            } else if (allBets[i].choice == 0) {
+                sumChoice0 += allBets[i].amount;
+            }
+        }
+        
+        product1 = sumChoice1 * odds1;
+        product2 = sumChoice2 * odds2;
+        product0 = sumChoice0 * odds0;
+
+        smallestChoice = product1; // Assume choice 1 is the smallest
+        uint256 smallestChoiceIndex = 1; // Index of choice 1
+
+        if (product2 < smallestChoice) {
+            smallestChoice = product2;
+            smallestChoiceIndex = 2;
+        }
+        
+        if (product0 < smallestChoice) {
+            smallestChoice = product0;
+            smallestChoiceIndex = 0;
+        }
+        
+        // Find the last bet index for the smallest choice
+        for (uint256 i = allBets.length - 1; i >= 0; i--) {
+            if (allBets[i].choice == smallestChoiceIndex) {
+                lastBetIndex = i;
+                break;
+            }
+        }
+    }
+
+
+
     /*function finalizeBetsAndRefund() public onlyOwner {
         isBettingOpen = false;
+        getChoiceSums();
 
         if (bettedAmount1 <= bettedAmount2 && bettedAmount1 <= bettedAmount0) {
-            bettedAmount2 -
+            uint returnsTo2 = bettedAmount2 - bettedAmount1;
+            uint returnsTo0 = bettedAmount0 - bettedAmount1;
 
+            
         } else if (bettedAmount2 <= bettedAmount1 && bettedAmount2 <= bettedAmount0) {
+            uint returnsTo1 = bettedAmount1 - bettedAmount2;
+            uint returnsTo0 = bettedAmount0 - bettedAmount2;
+
 
         } else {
-
-        }
-
-    } */
-
+            uint returnsTo1 = bettedAmount1 - bettedAmount0;
+            uint returnsTo2 = bettedAmount2 - bettedAmount0;
+            } 
+    }*/
     function getBetCount(address _user) public view returns (uint256) {
         return userBets[_user].length;
     }
@@ -91,11 +145,16 @@ contract OddsCalculator is ReentrancyGuard, Ownable {
         return (bet.choice, bet.amount);
     }
 
-    //function refund()
+    // Get all bets
+    function getAllBetsCount() public view returns (uint256) {
+        return allBets.length;
+    }
 
-        
+    function getAllBetDetails(uint256 _index) public view returns (uint256 choice, uint256 amount) {
+        Bet storage bet = allBets[_index];
+        return (bet.choice, bet.amount);
+    }
 }
-    
 
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
