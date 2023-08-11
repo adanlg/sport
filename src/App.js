23fe544f91bad7f50f84e7e5b17426ab49153a8e
adanlg <luisgadan88@gmail.com>
2023-08-11 02:51:09 +0200

    feat: refunds and winner

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const { ethers } = require("ethers");
 function App() {
   const [amount, setAmount] = useState(0);
   const [choice, setChoice] = useState(0);
+  const [winnerChoice, setWinnerChoice] = useState(0);
 
   useEffect(() => {
     // No es necesario el bloque "init" ya que no necesitas obtener una dirección de MetaMask aquí
@@ -49,11 +50,72 @@ function App() {
     }
   };
 
+  const finalizeBetsAndRefund = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        // Solicitar al usuario que autorice la conexión
+        await window.ethereum.enable();
+        
+        // Crear el proveedor y el signer
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(ContractAddress, ContractABI, signer);
+
+        try {
+          // Llamar a la función finalizeBetsAndRefund
+          const tx = await contract.finalizeBetsAndRefund();
+          await tx.wait();
+          console.log('Finalize bets and refund successful!');
+        } catch (error) {
+          console.error('Error finalizing bets and refund:', error);
+        }
+      } catch (error) {
+        console.error('Error connecting to MetaMask:', error);
+      }
+    } else {
+      console.log('MetaMask is not installed or enabled.');
+    }
+  };
+
+  const winner = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        // Solicitar al usuario que autorice la conexión
+        await window.ethereum.enable();
+        
+        // Crear el proveedor y el signer
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(ContractAddress, ContractABI, signer);
+
+        try {
+          // Llamar a la función winner con el parámetro _choice
+          const tx = await contract.winner(winnerChoice);
+          await tx.wait();
+          console.log('Winner function executed successfully!');
+        } catch (error) {
+          console.error('Error executing winner function:', error);
+        }
+      } catch (error) {
+        console.error('Error connecting to MetaMask:', error);
+      }
+    } else {
+      console.log('MetaMask is not installed or enabled.');
+    }
+  };
+
   return (
     <div>
       <input type="number" placeholder="Amount" value={amount} onChange={e => setAmount(e.target.value)} />
       <input type="number" placeholder="Choice" value={choice} onChange={e => setChoice(e.target.value)} />
       <button onClick={connectToContract}>Place Bet</button>
+
+      <button onClick={finalizeBetsAndRefund}>Finalize Bets and Refund</button>
+
+      <input type="number" placeholder="Winner Choice" value={winnerChoice} onChange={e => setWinnerChoice(e.target.value)} />
+      <button onClick={winner}>Declare Winner</button>
     </div>
   );
 }
